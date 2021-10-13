74384f44cc4b696c18ea1075fc30fe3624653f5a
SwiftKillers <yuri.silenok@gmail.com>
2021-10-13 08:50:54 +0300

    added visual studio project to the repo

---
+++ SomeApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SomeApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("some app.");
+        }
+    }
+}
